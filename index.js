c03a5dda34cdc9356200fc446837d6d1549b7816
3014zhangshuo <3014zhangshuo@gmail.com>
2019-02-11 10:27:27 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const Redis = require("ioredis")
+const Koa = require('koa')
+const Rounter = require('koa-router')
+
+const router = new Rounter()
+const app = new Koa()
+
+const redis = new Redis(process.env.REDIS_HOST)
+
+router.get('/', async (ctx, next) => {
+  await next()
+  await redis.incr('pv')
+  const current = await redis.get('pv')
+  ctx.body = `current pv: ${current}`
+})
+
+app.use(router.routes())
+  .use(router.allowedMethods())
+  .listen(4000)
